cb41d6109b6a2740037d3955ee7c63bf472520c6
YihanCai <143111860+YihanCai@users.noreply.github.com>
2024-01-17 10:48:14 +0800

    Add files via upload

--- bofan.cpp
+++ bofan.cpp
@@ -4,12 +4,28 @@
 #pragma comment(lib,"winmm.lib")
 
 int main() {
-	// 播放音乐
-	//首先，要把你要播放的歌与该文件放在同一文件夹下
-	//其次，代码打好后，还不够，要右键该项目，点击属性，点击配置属性中的高级，将字符集改为使用多字节字符集。
-	//再点击属性页的C/C++ ，在常规中把SDL 检查由 是 改为 否。
-	mciSendString("close Swimy.mp3",0,0,0);//close关闭播放的音乐，防止上次没有播放完。Swimy.mp3仅为示例。即歌名
-	mciSendString("play Swimy.mp3", 0, 0, 0);//play 开始播放音乐。
-	while (1);//无限循环，防止瞬间退出。
-	return 0;
+    char filename[100];
+
+    // Get user input for the music filename
+    printf("请输入你所要收听的音乐文件名（包括文件扩展名，如.mp3）:\n");
+    scanf("%s", filename);
+
+    // Construct and execute the MCI commands
+    char openCommand[256], playCommand[256];
+
+    // Close any currently playing music
+    sprintf(openCommand, "close %s", filename);
+    mciSendString(openCommand, 0, 0, 0);
+
+    // Open the specified music file with an alias
+    sprintf(openCommand, "open %s alias music", filename);
+    mciSendString(openCommand, 0, 0, 0);
+
+    // Play the music
+    sprintf(playCommand, "play music");
+    mciSendString(playCommand, 0, 0, 0);
+
+    while (1);
+    return 0;
 }
+
